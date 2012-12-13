4caca2a88e387fe49eb92ca592783f0b9c56f319
Ivan <ivan.a.hook@gmail.com>
2012-12-13 15:14:21 +0100

    lite metodskelett

--- src/Klient/Klient.java
+++ src/Klient/Klient.java
@@ -1,5 +1,39 @@
 package Klient;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Klient {
+	
+	private Socket connectionToServer;
+	private PrintWriter outStream;
+	private BufferedReader inStream;
+	
+	
+	//connect to server
+	public void connectToServer(String address, int port) {
+		
+	}
+	
+	//send message to server
+	public void send() {
+		
+	}
+	
+	//receive message from server
+	public void receive() {
+		
+	}
+	
+	//show message in text area
+	public void sendMessage(String m) {
+		
+	}
+	
+	//close streams and connections
+	public void close() {
+		
+	}
 
 }

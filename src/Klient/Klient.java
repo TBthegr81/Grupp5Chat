eea4ad388f3c6fcf7c12da8a35a2e2780dab7360
Ivan <ivan.a.hook@gmail.com>
2012-12-16 21:53:28 +0100

    helgarbete

--- src/Klient/Klient.java
+++ src/Klient/Klient.java
@@ -3,14 +3,18 @@ package Klient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import javax.swing.SwingUtilities;
 
 public class Klient {
 	
 	private Socket connectionToServer;
 	private PrintWriter outStream;
 	private BufferedReader inStream;
+	private String message = "";
 	
 	//connect to server
 	public void connectToServer(String address, int port) throws IOException {
@@ -26,22 +30,41 @@ public class Klient {
 	
 	//login to server
 	public void loginToServer() {
-		
+		connect();
+		receive();
+	}
+	
+	public void connect() {
+		try {
+			inStream = new BufferedReader(new InputStreamReader(connectionToServer.getInputStream()));
+			outStream = new PrintWriter(connectionToServer.getOutputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	//send message to server
-	public void send() {
-		
+	public void send(String m) {
+		outStream.write("" + m);
+		outStream.flush();
+		showMessage("" + m);
 	}
 	
 	//receive message from server
 	public void receive() {
-		
+		while(!message.equals("SERVER - END")){
+			message = (String) inStream.toString();
+			showMessage("\n" + message);		
+		}
 	}
 	
 	//show message in text area
 	public void showMessage(final String m) {
-		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				/*append text to chat window*/
+			}
+		});
 	}
 	
 	//close streams and connections

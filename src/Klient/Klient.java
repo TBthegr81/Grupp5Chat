a203baa4386f574cfc082e18752d43c68a39c5d7
Ivan <ivan.a.hook@gmail.com>
2013-01-08 12:34:52 +0100

    snart funkar objektskiten?

--- src/Klient/Klient.java
+++ src/Klient/Klient.java
@@ -14,37 +14,45 @@ import javax.swing.SwingUtilities;
 public class Klient {
 
 	private Socket connection;
-//	private PrintWriter outStream;
+	//	private PrintWriter outStream;
 	private ObjectOutputStream outStream;
 	private ObjectInputStream inStream;
-//	private BufferedReader inStream;
-//	private FileWriter output;
+	//	private BufferedReader inStream;
+	//	private FileWriter output;
 	private String message = "";
 	private String user;
 	private String address;
-	private String userName;
+	private String userName = "klient";
 	private int answer;
 	private int port;
+	private int id;
 
 	//konstruktor
-	Klient() {
+	Klient()
+	{
 	}
 
-	public void startRunning() {
+	public void startRunning()
+	{
 		address = "10.0.0.1";
 		port = 54602;
 		connect(address, port);
-		receive();
+		try {
+			receive();
+		} catch (ClassNotFoundException | IOException e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
 	}
 
 	//connect to server and setup streams
-	public void connect(String address, int port) {
+	public void connect(String address, int port)
+	{
 		System.out.println("connecting to: " + address + "...");
 		try {
 			connection = new Socket(address, port);
-//			inStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			//			inStream = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			//			outStream = new PrintWriter(connection.getOutputStream());
 			inStream = new ObjectInputStream((connection.getInputStream()));
-//			outStream = new PrintWriter(connection.getOutputStream());
 			outStream = new ObjectOutputStream(connection.getOutputStream());
 		} catch (IOException e) {
 			System.err.println("ERROR: " + e.getMessage());
@@ -53,85 +61,90 @@ public class Klient {
 	}
 
 	//checks the output from the gui input field then sends if appropriate
-	public void checkMessage(String m) {
+	public void checkMessage(String m)
+	{
 		switch(m) {
 		case "/dc":
-			send(answer + " " + "klient" + " " + m);
+			send(answer, userName, m);
 			close();
 		default:
-			send(answer + " " + "klient" + " " + m);
+			send(answer, userName, m);
 		}
 	}
-	
+
 	//chose username for current session
-	public void choseUsername() {
+	public void choseUsername()
+	{
 		userName = Main.gui.messageInputField.getText();
 	}
-	
-//	public void writeFile(String fileName, String textToWrite) throws IOException {
-//		//open output stream
-//		output = new FileWriter(fileName);
-//		
-//		try{
-//			output.write(textToWrite);
-//		}catch(IOException e) {
-//			System.err.println("ERROR: " + e.getMessage());
-//		}
-//		
-//		//close output stream
-//		try{
-//			output.close();
-//		}catch(IOException e) {
-//			System.err.println("ERROR: " + fileName + e.getMessage());
-//		}
-//	}
+
+	//	public void writeFile(String fileName, String textToWrite) throws IOException {
+	//		//open output stream
+	//		output = new FileWriter(fileName);
+	//		
+	//		try{
+	//			output.write(textToWrite);
+	//		}catch(IOException e) {
+	//			System.err.println("ERROR: " + e.getMessage());
+	//		}
+	//		
+	//		//close output stream
+	//		try{
+	//			output.close();
+	//		}catch(IOException e) {
+	//			System.err.println("ERROR: " + fileName + e.getMessage());
+	//		}
+	//	}
 
 	//send message to server
-	public void send(String m) {
-		outStream.println(m);
-		outStream.flush();
+	public void send(int id, String userName, String message)
+	{
+		Message outMessage = new Message(id, userName, message);
+		try {
+			outStream.writeObject(outMessage);
+			outStream.flush();
+		} catch (IOException e) {
+			System.err.println("ERROR: " + e.getMessage());
+		}
 	}
 
 	//receive message from server
-	public void receive() {
+	public void receive() throws ClassNotFoundException, IOException
+	{
 		//while message is not SERVER - END, keep receiving
+		Message inputMessage = null;
 		do {
-			try {
-//				System.out.println(inStream.readLine() + "\n");
-				String fromServer[] = inStream.readLine().split("\\s+",3);
-				//answer takes the int that decides what state is to be used in answerCase()
-				answer = Integer.parseInt(fromServer[0]);
-				user = fromServer[1];
-				message = fromServer[2];
-				System.out.println(answer + " " + user + " " + message);
-				answerCase(answer, user, message);
-				Main.gui.showReceivedMessage(message, user);
-			}catch (IOException e) {
-				System.err.println("ERROR: " + e.getMessage());
-			}
-		}while(!message.equals("SERVER - END")); //annan lösning här såklart, eller?
+			inputMessage = (Message) inStream.readObject();
+			//answer takes the int that decides what state is to be used in answerCase()
+			answer = inputMessage.getId();
+			user = inputMessage.getUsername();
+			message = inputMessage.getMessage();
+			System.out.println(answer + " " + user + " " + message);
+			answerCase(answer, message);
+			Main.gui.showReceivedMessage(message, user);
+		}while((inputMessage = (Message) inStream.readObject()) != null);
 		close();
 	}
 
-
-	public void answerCase(int answer, String user, String message) {
+	public void answerCase(int answer, String user)
+	{
 		switch(answer){
 		case 1:			//connected to server, got welcome message
-			send(answer + " " + "klient" + " " + "Ted");
+			send(answer, "Ted", "hej");
 			break;
 		case 2:			//username accepted
-			send(answer + " " + "klient" + " " + "OK");
+			send(answer, userName, "OK");
 			break;
 		case 3:			//username not accepted... get new from GUI
-			send(answer + " " + "klient" + " " + "FlowDan");
+			send(answer, "FlowDaN", "lala");
 			break;
 		case 4:			//got MOT
-			send(answer + " " + "klient" + " " + "OK");
+			send(answer, userName, "OK");
 			break;
 		case 5:			//list of users
 			chopStrings(message);
-//			fillUsers(message);
-			send(answer + " " + "klient" + " " + "OK");
+			//			fillUsers(message);
+			send(answer, userName, "OK");
 			break;
 		case 6:			//list of rooms
 			Main.gui.messageInputField.setEditable(true);
@@ -147,7 +160,8 @@ public class Klient {
 		}
 	}
 
-	public String[] chopStrings(String m) {
+	public String[] chopStrings(String m)
+	{
 		String serverInfo[] = m.split("\\s+");
 		for(int i = 0; i < serverInfo.length; i++) {
 			System.out.println(serverInfo[i] + " ");
@@ -155,12 +169,14 @@ public class Klient {
 		return serverInfo;
 	}
 
-	public void fillUsers(String m) {
+	public void fillUsers(String m)
+	{
 		/*Main.gui.usersWindow*/chopStrings(m);
 	}
 
 	//close streams and connections
-	public void close() {
+	public void close()
+	{
 		System.out.println("closing connections");
 		try {
 			inStream.close();

bb5cb93507c53cd8b54c7b74997ca61220de20cc
Joakim Lavenius <theguidingblue@gmail.com>
2013-01-14 14:58:17 +0100

    Update src/Klient/About.java
    Manuell update

--- src/Klient/About.java
+++ src/Klient/About.java
@@ -1,1 +1,93 @@
-//Hej
+package Klient;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextPane;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.UIManager;
+
+public class About extends JFrame {
+
+  private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	
+	//Till för att stänga fönster
+	private void windowCloser(){
+		this.setVisible(false);
+	}
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					About frame = new About();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public About() {
+		setMinimumSize(new Dimension(200, 200));
+		setMaximumSize(new Dimension(200, 200));
+		setSize(new Dimension(200, 200));
+		setPreferredSize(new Dimension(200, 200));
+		setTitle("About");
+		setResizable(false);
+		setBounds(100, 100, 200, 200);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{15, 139, 15, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0};
+		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JTextPane textArea = new JTextPane();
+		textArea.setBackground(UIManager.getColor("Button.background"));
+		textArea.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		textArea.setText("iKlient and iServer\r\n\r\nTorbj\u00F6rn Hagenstam\r\nJoakim Lavenius\r\nIvan H\u00F6\u00F6k\r\n\r\nNo rights reserved.");
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 3;
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		panel.add(textArea, gbc_textArea);
+		
+		JButton okButton = new JButton("Ok");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridwidth = 3;
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 1;
+		panel.add(okButton, gbc_btnNewButton);
+		
+		okButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				windowCloser();
+			}
+		});
+	}
+}
+

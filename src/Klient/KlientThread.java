6322c406955aed479d848b5f49042058714fcb64
Joakim Lavenius <theguidingblue@gmail.com>
2013-01-14 15:00:21 +0100

    Update src/Klient/KlientThread.java

--- src/Klient/KlientThread.java
+++ src/Klient/KlientThread.java
@@ -1,1 +1,19 @@
-//Hej
+package Klient;
+
+public class KlientThread implements Runnable {
+
+//  private int port;
+//	private String address;
+//	
+//	KlientThread() 
+//	{
+//		int port = 54602;
+//		String address = "10.0.0.1";
+//	}
+
+	@Override
+	public void run() {
+		Main.klient.startRunning("10.0.0.1", 54602);
+	}
+
+}
